4d743bbafc2208eed9c76b3541d43e5f6d1b263f
navya3377 <navya3377@gmail.com>
2026-04-28 12:01:19 +0530

    add

--- pythonsample.py
+++ pythonsample.py
@@ -1,10 +1,7 @@
 name = "Pratyusha"
 age = 25
 city= "Hyderabad"
-salary = 30000
-name = "Pratyusha"
-age = 25
-city= "Hyderabad" 
+salary = 30000 
 print(name, age,city,salary)
 print("Team collaboration!")
 
