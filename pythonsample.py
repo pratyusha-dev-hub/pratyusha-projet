10bd7f73f0228e9ebbd7ff553b06916ddb669b77
navya3377 <navya3377@gmail.com>
2026-04-28 15:20:41 +0530

    ADD

--- pythonsample.py
+++ pythonsample.py
@@ -5,5 +5,10 @@ salary = 30000
 print(name, age,city,salary)
 print("Team collaboration!")
 print("i am creating the conflict to resolve")
+<<<<<<< HEAD
+print("Hello from branch2")
+=======
+print("Hello from branch1")
+>>>>>>> branch1
 
 
