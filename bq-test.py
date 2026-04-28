426aa58629a9a467918e68ea0c5a3c582f95db6e
Pratyusha <pratyusha@email.com>
2026-04-28 17:09:08 +0530

    Pratyusha change

--- bq-test.py
+++ bq-test.py
@@ -2,6 +2,6 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 print([dataset.dataset_id for dataset in client.list_datasets()])
-print("BigQuery connection successful!") 
+print("BigQuery connection successful!")
 print("Change from Pratyusha")
 print("Change from colleague")
